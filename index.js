32a3bc8b64c93dedd4b89da37e5b135741c3b8bb
MaxMEllon <maxmellon1994@gmail.com>
2018-02-18 21:33:28 +0900

    Checking each arguments types.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = funcs => funcs.reverse().reduce((g, f) => x => g(f(x)))
+const isFunc = maybeFunc => {
+  if (typeof maybeFunc === 'function') return true
+  throw new Error('`compositter` expected Array<Function> type.')
+}
+
+module.exports = funcs => funcs.reverse().filter(isFunc).reduce((g, f) => x => g(f(x)))
